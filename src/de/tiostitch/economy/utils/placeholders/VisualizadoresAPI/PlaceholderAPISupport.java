f19503530ffba4b3937b6c2b0a5db78bf3630ec8
TioStitch <87840489+TioStitch@users.noreply.github.com>
2023-08-25 19:49:03 -0300

    Delete src directory

--- src/de/tiostitch/economy/utils/placeholders/VisualizadoresAPI/PlaceholderAPISupport.java
+++
@@ -1,9 +0,0 @@
-package de.tiostitch.economy.utils.placeholders.VisualizadoresAPI;
-
-import org.bukkit.entity.Player;
-
-public interface PlaceholderAPISupport {
-  String chat(Player paramPlayer, String paramString);
-
-  String chatApiOnly(Player paramPlayer, String paramString);
-}
